f4f8d58daacc27b8e85d685e6d173b4b145c8af4
rain <zhuyu@think-land.com>
2017-08-04 10:21:12 +0800

    use schema for data check

--- app/auth/login.py
+++ app/auth/login.py
@@ -4,6 +4,7 @@ from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from ..response import BaseResponse
+from schema import Schema
 
 
 @auth.route('/login', methods=['POST'])
@@ -11,9 +12,12 @@ def login():
     email = request.form.get('email')
     passwd = request.form.get('password')
 
-    if not email or not passwd:
-        current_app.logger.error(u"登录失败: emial或passwd未传")
-        return BaseResponse(code='400001').get_response()
+    Schema(basestring).validate(email)
+    Schema(basestring).validate(passwd)
+
+    # if not email or not passwd:
+    #     current_app.logger.error(u"登录失败: emial或passwd未传")
+    #     return BaseResponse(code='400001').get_response()
 
     user = User.query.filter_by(email=email).first()
     if user is not None and user.verify_password(passwd):
